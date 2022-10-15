a95d1ede58b85a9cd7ba21c6385f22ae31c0d768
Joshua Walderbach <iamnotzerocool@users.noreply.github.com>
2022-10-15 09:05:16 -0500

    Update Remove_3p_4p_Daphne_Lightgun.ps1
    Added UCS paths to be removed too

--- Remove_3p_4p_Daphne_Lightgun.ps1
+++ Remove_3p_4p_Daphne_Lightgun.ps1
@@ -5,6 +5,7 @@ Get-ChildItem -Path "D:\cox\logo\*_3p.*" | Remove-Item -Verbose
 Get-ChildItem -Path "D:\cox\marquee\*_3p.*" | Remove-Item -Verbose
 Get-ChildItem -Path "D:\cox\playlist_art\2 3-Player.png" | Remove-Item -Verbose
 Get-ChildItem -Path "D:\cox\playlists\2 3-Player.txt" | Remove-Item -Verbose
+Get-ChildItem -Path "D:\3 Player Build" | Remove-Item -Verbose
 
 #4 Player
 Get-ChildItem -Path "D:\cox\cover\*_4p.*" | Remove-Item -Verbose
@@ -13,6 +14,7 @@ Get-ChildItem -Path "D:\cox\logo\*_4p.*" | Remove-Item -Verbose
 Get-ChildItem -Path "D:\cox\marquee\*_4p.*" | Remove-Item -Verbose
 Get-ChildItem -Path "D:\cox\playlist_art\2 4 Player.png" | Remove-Item -Verbose
 Get-ChildItem -Path "D:\cox\playlists\2 4 Player.txt" | Remove-Item -Verbose
+Get-ChildItem -Path "D:\4 Player Build" | Remove-Item -Verbose
 
 #Daphne
 Get-ChildItem -Path "D:\cox\cover\*_Daphne.*" | Remove-Item -Verbose
@@ -21,7 +23,9 @@ Get-ChildItem -Path "D:\cox\logo\*_Daphne.*" | Remove-Item -Verbose
 Get-ChildItem -Path "D:\cox\marquee\*_Daphne.*" | Remove-Item -Verbose
 Get-ChildItem -Path "D:\cox\playlist_art\2 5 Daphne.png" | Remove-Item -Verbose
 Get-ChildItem -Path "D:\cox\playlists\2 5 Daphne.txt" | Remove-Item -Verbose
+Get-ChildItem -Path "D:\content" | Remove-Item -Verbose
 
 #Lightgun
 Get-ChildItem -Path "D:\cox\playlist_art\2 Lightgun.png" | Remove-Item -Verbose
 Get-ChildItem -Path "D:\cox\playlists\2 Lightgun.txt" | Remove-Item -Verbose
+Get-ChildItem -Path "D:\Lightgun Build" | Remove-Item -Verbose
