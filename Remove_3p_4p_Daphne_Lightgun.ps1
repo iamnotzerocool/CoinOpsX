b3f28e6b9e2408090bb99ab9a95325bca01b2413
Joshua Walderbach <iamnotzerocool@users.noreply.github.com>
2022-10-15 08:56:09 -0500

    Add files via upload

---
+++ Remove_3p_4p_Daphne_Lightgun.ps1
@@ -0,0 +1,27 @@
+#3 Player
+Get-ChildItem -Path "D:\cox\cover\*_3p.*" | Remove-Item -Verbose
+Get-ChildItem -Path "D:\cox\custom\*_3p.png" | Remove-Item -Verbose
+Get-ChildItem -Path "D:\cox\logo\*_3p.*" | Remove-Item -Verbose
+Get-ChildItem -Path "D:\cox\marquee\*_3p.*" | Remove-Item -Verbose
+Get-ChildItem -Path "D:\cox\playlist_art\2 3-Player.png" | Remove-Item -Verbose
+Get-ChildItem -Path "D:\cox\playlists\2 3-Player.txt" | Remove-Item -Verbose
+
+#4 Player
+Get-ChildItem -Path "D:\cox\cover\*_4p.*" | Remove-Item -Verbose
+Get-ChildItem -Path "D:\cox\custom\*_4p.png" | Remove-Item -Verbose
+Get-ChildItem -Path "D:\cox\logo\*_4p.*" | Remove-Item -Verbose
+Get-ChildItem -Path "D:\cox\marquee\*_4p.*" | Remove-Item -Verbose
+Get-ChildItem -Path "D:\cox\playlist_art\2 4 Player.png" | Remove-Item -Verbose
+Get-ChildItem -Path "D:\cox\playlists\2 4 Player.txt" | Remove-Item -Verbose
+
+#Daphne
+Get-ChildItem -Path "D:\cox\cover\*_Daphne.*" | Remove-Item -Verbose
+Get-ChildItem -Path "D:\cox\custom\*_Daphne.png" | Remove-Item -Verbose
+Get-ChildItem -Path "D:\cox\logo\*_Daphne.*" | Remove-Item -Verbose
+Get-ChildItem -Path "D:\cox\marquee\*_Daphne.*" | Remove-Item -Verbose
+Get-ChildItem -Path "D:\cox\playlist_art\2 5 Daphne.png" | Remove-Item -Verbose
+Get-ChildItem -Path "D:\cox\playlists\2 5 Daphne.txt" | Remove-Item -Verbose
+
+#Lightgun
+Get-ChildItem -Path "D:\cox\playlist_art\2 Lightgun.png" | Remove-Item -Verbose
+Get-ChildItem -Path "D:\cox\playlists\2 Lightgun.txt" | Remove-Item -Verbose
